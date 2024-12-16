6ccb1cf000eb9c804377bc65301962ff42030b85
JefersonManosalvas <jeferson.manosalvas979@ist17dejulio.edu.ec>
2024-12-16 10:49:17 -0500

    ejercicio

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-const express=require('express');
-const app = express;
-app.use(express.json());
-var lista=[
-{
-    id:'1',
-    nombre:'jeferson',
+const express = require('express'); 
+const app = express(); 
 
-},
-{
-    id:'2',
-    nombre:'paul',
+app.use(express.json()); 
 
-}];
 
-//uso de app, web service de tipo get
+var lista = [
+  {
+    id: '1',
+    nombre: 'Jeferson', 
+  },
+  {
+    id: '2',
+    nombre: 'Paul',
+  },
+];
 
-app.length('/nombre',(req,resp)=>{
-    resp.json(lista);
+
+app.get('/nombre', (req, resp) => {
+  resp.json(lista);
 });
 
-app.listen(3000,()=>{
-    console.log('el puero 3000 esta corriendo');
+app.get('/jeferson',(req, resp)=>{
+    resp.send('Mi nombre es Manosalvas Jeferson tengo 21 años soy estudiante del 17 de julio de la carrera de softeare');
 });
+
+app.get('/suma',(req, resp)=>{
+    let n1= 10;
+    let n2 = 2;
+    resp.send(''+(n1+n2));
+});
+
+app.get('/sumaCliente/:n1',(req, resp)=>{
+    let num1= parseInt(req.params.n1);
+    let n2 = 2;
+    let suma=num1+n2;
+    resp.send(''+suma);
+});
+
+// Iniciar servidor en el puerto 3000
+app.listen(3000, () => {
+  console.log('El puerto 3000 está corriendo'); 
+});
+
 
